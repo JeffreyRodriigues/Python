51ac93898cbdd6952ac84a6e82fc67f2720a2255
Jeffrey Rodrigues <jeffrey.rodriigues@gmail.com>
2023-01-06 18:26:15 -0300

    about While

---
+++ Basic/aula33.py
@@ -0,0 +1,11 @@
+"""
+https://docs.python.org/3/library/stdtypes.html
+Imútaveis que vimos: str, int, float, bool
+"""
+string = "luiz Otávio"
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# string[3] = 'ABC'
+# print(string)
+# print(outra_variavel)
+# print(string.capitalize())
+print(string.zfill(50))
